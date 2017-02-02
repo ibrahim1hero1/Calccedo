742162befb3f1427e4e6a127fb6a7d85e7ae6411
ibrahim1hero1 <ibrahim_hero1@yahoo.com>
2017-02-02 10:52:21 -0800

    Create Infinity.java

---
+++ com/precious/calccedo/handlers/Infinity.java
@@ -0,0 +1,35 @@
+
+/*
+ *   Calccedo Library
+ *   hint this code under Apache License
+ */
+
+
+
+package com.precious.calccedo.handlers;
+
+import com.precious.calccedo.configuration.Configuration;
+
+/**
+ *
+ * @author Ibrahim Abdsaid Hanna
+ *         ibrahim.seniore@gmail.com
+ */
+public class Infinity {
+    
+    
+    public Infinity(){
+    
+        
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "This class to help developers when custom operand divided by zero or not legal arithmatic";
+    }
+    
+    
+    
+    
+}
