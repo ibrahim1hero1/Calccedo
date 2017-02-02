0d7ecb38a9d92148f9aa0af0bb65047aa867271d
ibrahim1hero1 <ibrahim_hero1@yahoo.com>
2017-02-02 10:53:30 -0800

    Create QuoteResult.java

---
+++ com/precious/calccedo/handlers/QuoteResult.java
@@ -0,0 +1,32 @@
+
+/*
+ *   Calccedo Library
+ *   hint this code under Apache License
+ */
+
+
+package com.precious.calccedo.handlers;
+
+/**
+ *
+ * @author Ibrahim Abdsaid Hanna
+ *         ibrahim.seniore@gmail.com
+ */
+public class QuoteResult {
+    
+     
+     int offsetA;
+     int offsetZ;
+     double result;
+
+    public QuoteResult(int offsetA, int offsetZ, double result) {
+        this.offsetA = offsetA;
+        this.offsetZ = offsetZ;
+        this.result = result;
+    }
+     
+     
+     
+     
+    
+}
