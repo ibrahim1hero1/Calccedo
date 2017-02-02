acb5f13ebcdb8a6cf58b1dc13799eef55160b148
ibrahim1hero1 <ibrahim_hero1@yahoo.com>
2017-02-02 10:49:37 -0800

    Create CustomOperandMap.java

---
+++ com/precious/calccedo/handlers/CustomOperandMap.java
@@ -0,0 +1,56 @@
+/*
+ *   Calccedo Library
+ *   hint this code under Apache License
+ */
+
+
+
+
+package com.precious.calccedo.handlers;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Ibrahim Abdsaid Hanna
+ *         ibrahim.seniore@gmail.com
+ */
+
+
+public class CustomOperandMap {
+    
+  public static HashMap<String, Object> customOperandMap_Constant;
+    
+    
+    public  CustomOperandMap(){
+        customOperandMap_Constant=new HashMap<>();
+    }
+    
+    
+    
+    
+    public void add(String customOperand, int quoteNumber,double value){
+      customOperandMap_Constant.put(customOperand+quoteNumber, new CustomOperand(customOperand+quoteNumber, value));
+    }
+    
+    
+    public  void add(String customOperand, double quoteNumber,double value){
+         customOperandMap_Constant.put(customOperand+quoteNumber, new CustomOperand(customOperand+quoteNumber, value));
+    }
+    
+    
+    public  void add(String customOperand, int quoteNumber,Object object){
+         customOperandMap_Constant.put(customOperand+quoteNumber, object);
+    }
+    
+    public  void add(String customOperand, double quoteNumber,Object object){
+         customOperandMap_Constant.put(customOperand+quoteNumber, object);
+    }
+    
+    
+     public HashMap<String,Object> getcustomOperandMap_Constant(){
+         return customOperandMap_Constant;
+     }   
+    
+    
+}
