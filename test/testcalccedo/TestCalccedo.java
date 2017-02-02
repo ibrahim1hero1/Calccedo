8343267ca5dbb5d9fd8e043d343bd0bb87aa9991
ibrahim1hero1 <ibrahim_hero1@yahoo.com>
2017-02-02 15:37:56 -0800

    Create TestCalccedo.java

---
+++ test/testcalccedo/TestCalccedo.java
@@ -0,0 +1,43 @@
+
+
+/*
+  This is simple code to help how to use Calccedo Engine Library
+
+*/
+package testcalccedo;
+
+import com.precious.calccedo.handlers.CalccedoHandler;
+
+
+
+
+/**
+ *
+ * @author Ibrahim Abdsaid Hanna
+ *         ibrahim.seniore@gmail.com
+ */
+
+public class TestCalccedo extends CalccedoHandler{
+
+    public TestCalccedo(){
+        super.init();
+        // to show tracing operations of Calccedo
+        super.enableDeepTrace(true);
+        
+    }
+    
+    
+    
+    public static void main(String[] args) {
+       // this is sample formula  
+       String formula="1+1+2+3Sin(30)+10";
+       
+       // instantiate object from CalccedoHandler
+       CalccedoHandler calccedoHandler=new CalccedoHandler();
+       
+       // now let's get result, just by calling calccedo cslculate method
+       System.out.println("Welcome to Calccedo Engine Library");
+       System.out.println("Result is "+calccedoHandler.calculate(formula));
+       
+    }  
+}
