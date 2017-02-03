befc76e41d8adf452bf57b8e2137c1d79ec9a135
ibrahim1hero1 <ibrahim_hero1@yahoo.com>
2017-02-03 08:25:45 -0800

    Update CustomOperandException.java

--- com/precious/calccedo/handlers/CustomOperandException.java
+++ com/precious/calccedo/handlers/CustomOperandException.java
@@ -26,7 +26,7 @@ public class CustomOperandException extends RuntimeException{
              System.out.println("Computer calculation different than simple calculator just because,");
              System.out.println("Computer RAM size bigger than simple calculator, so you can find some equations should return infinity result, but in computer return value");
              System.out.println("don't worry, Calccedo Library help you to custome any equation that has constant value");
-             System.out.println("please read Calccedo documentation or contact me on www.github.com/ibrahim1hero1/calccedo \n");
+             System.out.println("please read Calccedo documentation or contact me on www.github.com/ibrahim1hero1/Calccedo \n");
          }
 
         System.err.println("Exception:");    
