9f202f6c00dd2c6c046434b86f965e2a8fae8b07
Ibrahim Abdsaid Hanna <ibrahim_hero1@yahoo.com>
2017-08-21 23:04:58 +0200

    Update Eshortuct.java

--- com/precious/calccedo/handlers/Eshortuct.java
+++ com/precious/calccedo/handlers/Eshortuct.java
@@ -1,11 +1,14 @@
 
 /*
  *   Calccedo Library
- *   hint this code under Apache License
+ *   hint this code under ApacheLicense
  */
 
 
 package com.precious.calccedo.handlers;
+
+import com.precious.calccedo.configuration.Configuration;
+import java.util.ArrayList;
 
 
 
@@ -15,15 +18,16 @@ package com.precious.calccedo.handlers;
  * @author Ibrahim Abdsaid Hanna
  *         ibrahim.seniore@gmail.com
  */
-public class Eshortuct extends CalccedoHandler{
+ class Eshortuct extends CalccedoHandler{
     
      String leftTemp;
      char operandType;
      int leftOperandIndex;
      int rightOperandIndex;
-   
+     ArrayList<Character> list=new ArrayList<Character>();
+     
+     
         public Eshortuct(){
-            
         }
      
     public Eshortuct(String leftTemp,char operandType,int leftOperandIndex,int rightOperandIndex){
@@ -31,6 +35,8 @@ public class Eshortuct extends CalccedoHandler{
         this.operandType=operandType;
         this.leftOperandIndex=leftOperandIndex;
         this.rightOperandIndex=rightOperandIndex;
+        
+        
     }
 
  
@@ -43,7 +49,11 @@ public class Eshortuct extends CalccedoHandler{
          int rightOperandIndex = 0;
         
          boolean ignore=false;
-         
+         list.clear();
+         list.add('/');
+         list.add('%');
+         list.add('*');
+         list.add('^');
          
          
            for(int i=Eindex;i>=0;i--){
@@ -57,7 +67,10 @@ public class Eshortuct extends CalccedoHandler{
                       leftOperandIndex=i;
                       break;
                    }
-                   
+                    else if(list.contains(subformula.charAt(i))){
+                      leftOperandIndex=i;
+                      break;
+                    }
                }
                else{
                    leftTemp=subformula.charAt(i)+leftTemp;
@@ -69,7 +82,7 @@ public class Eshortuct extends CalccedoHandler{
            for(int j=Eindex;j<subformula.length();j++){
                
                 if(!isNumber(subformula.charAt(j))){
-                   if(isMinus(subformula.charAt(j))&&Eindex+1=='-'&&ignore==false){
+                   if(isMinus(subformula.charAt(j))&&subformula.charAt(j-1)=='E'&&ignore==false){
                        leftTemp=leftTemp+'-';
                        ignore=true;
                    }
@@ -90,24 +103,31 @@ public class Eshortuct extends CalccedoHandler{
                        break;
                   }
                  
+                   else if(list.contains(subformula.charAt(j))){
+                       operandType=subformula.charAt(j);
+                       rightOperandIndex=j;
+                       break;
+                  }
                }
                else{
                    leftTemp=leftTemp+subformula.charAt(j);
                }
            }
-         
-        return new Eshortuct(leftTemp, operandType,leftOperandIndex,rightOperandIndex);
+       if(Configuration.deepTracing)
+         System.out.println("leftTemp:"+leftTemp+"\noperandType:"+operandType+"\nleftOperandIndex"+leftOperandIndex+"\nrightOperandIndex"+rightOperandIndex+"\n");
+      
+       return new Eshortuct(leftTemp, operandType,leftOperandIndex,rightOperandIndex);
     }
     
     
     
     public boolean isMinus(char c){
-        return c=='-' ? true:false;
+        return c=='-';
     }
     
     
     public boolean isPlus(char c){
-        return c=='+' ? true:false;
+        return c=='+';
     }
     
 
