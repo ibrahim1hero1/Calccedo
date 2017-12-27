9b765653268c3aecb3bef2b531d3199cbb8b166e
Ibrahim Abdsaid Hanna <ibrahim_hero1@yahoo.com>
2017-12-28 00:25:55 +0200

    Update Configuration.java

--- com/precious/calccedo/configuration/Configuration.java
+++ com/precious/calccedo/configuration/Configuration.java
@@ -19,16 +19,37 @@ import com.precious.calccedo.handlers.Infinity;
  */
   public abstract class Configuration {
     
-      public static boolean deepTracing=false; 
-      CustomOperandMap constantOperand=new CustomOperandMap();
+    /**
+     *boolean variable to print tracing acts or not
+     */
+    public static boolean deepTracing=false; 
      
-      public Class getCustomeOperandMap(){
-          return CustomOperandMap.class;
-      }
-    // rename list and list2 here to make calccedo handler clear design
-      protected void init(){
+     /**
+     *object from CustomOperandMap to handle and hold all custom operands added by calccedo or added by developers 
+     */
+    CustomOperandMap customOperandMap;
+
+    /**
+     *boolean variable to avoid repeat super constructor calling of init() method 
+     *just by assigned it to true 
+     *
+     */
+    public static boolean closeGate = false;
+
+    /**
+     *Constructor of Configuration class which instantiate CustomOperandMap while calling
+     */
+    public Configuration() {
+        this.customOperandMap = new CustomOperandMap();
+    }
+
+    /**
+     *This method work as super method in calccedo, it is run once and do all configurations
+     *needed in calccedo to help subclasses do the right calculation 
+     */
+    protected void init(){
        
-       // Calccedo initlization 
+       if(!closeGate){ 
         addCustomOperand("Sin",30, 0.5);
         addCustomOperand("Sin",30.0, 0.5);
         addCustomOperand("Cos",60, 0.5);
@@ -37,32 +58,69 @@ import com.precious.calccedo.handlers.Infinity;
         addCustomOperand("Cos",90.0, 0);
         addCustomOperand("Tan",45,1);
         addCustomOperand("Tan",45.0,1);
-        addCustomOperand("Tan",90,new Infinity());
-        addCustomOperand("Tan",90.0,new Infinity());
-       
-        
+        addCustomOperand("Tan",90,Infinity.class);
+        addCustomOperand("Tan",90.0,Infinity.class);      
+        closeGate=true; 
+       }
       }
     
+     /**
+     * This method to help in tracing calccedo acts step by step if it set to true 
+     * @param enabled true | false
+     */
     protected void enableDeepTrace(boolean enabled){
         deepTracing=enabled;
     }
       
+    /**
+     *this method to add custom operand that handle integer value as a fact like Sin(30)=0.5 
+     * @param customOperand   operand name>>>>>> Sin
+     * @param quoteNumber     argument as int >>>>>> 30
+     * @param value           value that custom operand should produce  >>>>>> 0.5
+     */
     protected void addCustomOperand(String customOperand, int quoteNumber,double value){
-      constantOperand.add(customOperand, quoteNumber, value);
+      customOperandMap.add(customOperand, quoteNumber, value);
     }
     
-    
+    /**
+     *this method to add custom operand that handle double value as a fact like Sin(30.0)=0.5 
+     * @param customOperand   operand name>>>>>> Sin
+     * @param quoteNumber     argument as double >>>>>> 30.0
+     * @param value           value that custom operand should produce >>>>>> 0.5
+     */
     protected void addCustomOperand(String customOperand, double quoteNumber,double value){
-      constantOperand.add(customOperand, quoteNumber, value);
+      customOperandMap.add(customOperand, quoteNumber, value);
     }
       
+    /**
+     *this method to add infinity fact to custom operand that handle integer value like Tan(90)= Error,Infinity
+     * @param customOperand   operand name>>>>>> Tan
+     * @param quoteNumber     argument number int >>>>>> 90
+     * @param object          value that custom operand should produce infinity.class
+     */
     protected void addCustomOperand(String customOperand, int quoteNumber, Object object){
-      constantOperand.add(customOperand, quoteNumber, object);
+      customOperandMap.add(customOperand, quoteNumber, object);
     }
       
-    
+    /**
+     *this method to add infinity fact to custom operand that handle double value Tan(90.0)= Error,Infinity
+     * @param customOperand  operand name>>>>>> Tan 
+     * @param quoteNumber    argument number double >>>>>> 90.0
+     * @param object         value that custom operand should produce infinity.class
+     */
     protected void addCustomOperand(String customOperand, double quoteNumber, Object object){
-      constantOperand.add(customOperand, quoteNumber, object);
+      customOperandMap.add(customOperand, quoteNumber, object);
     }
-      
+
+    /**
+     *
+     * @return This is configuration class of calccedo which load predefined custom operands and predefined variables
+     */
+    @Override
+    public String toString() {
+        return "This is configuration class of calccedo which load predifiend custom operands and predefined variables";
+    }
+    
+   
 }
+
