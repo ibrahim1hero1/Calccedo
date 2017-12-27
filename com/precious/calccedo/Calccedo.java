e9dfa2a58388b9ae0f6131dd446f14fe3e1423c0
Ibrahim Abdsaid Hanna <ibrahim_hero1@yahoo.com>
2017-12-28 00:24:43 +0200

    Update Calccedo.java

--- com/precious/calccedo/Calccedo.java
+++ com/precious/calccedo/Calccedo.java
@@ -8,41 +8,38 @@
 
 import com.precious.calccedo.configuration.Configuration;
 
-
-
-
-
-
 /**
- *
  * @author Ibrahim Abdsaid Hanna
  *         ibrahim.seniore@gmail.com
  */
-  public abstract class Calccedo extends Configuration{
-    
-    
- 
-    @Override
-    protected void init() {
+public abstract class Calccedo extends Configuration{
+
+    /**
+     *Constructor of Calccedo which execute init() method in Configuration class while calling
+     */
+    public Calccedo() {
         super.init();
     }
-
+    
+    /**
+     * This method to help in tracing calccedo acts step by step if it set to true 
+     * @param enabled true | false
+     */
     @Override
-    public void enableDeepTrace(boolean enabled) {
+    protected void enableDeepTrace(boolean enabled) {
         super.enableDeepTrace(enabled); 
     }
 
+    /**
+     * @return Calccedo Library is calculation engine library designed to help developers for building applications that need complecated Mathematical equations, 
+     *         Calccedo is not just library, it's work as interperter and act as AI engines
+     */
     @Override
     public String toString() {
         return " Calccedo Library is calculation engine library designed to help developers for building applications that need complecated Mathematical equations,"
                 + "\n  Calccedo is not just library, it's work as interperter and act as AI engines";
     }
-    
-    
-    
-   
-   
-    
-    
+     
     
 }
+
