bcc18f1ff4cd40089da5fa6386173989a19325d9
Ibrahim Abdsaid Hanna <ibrahim_hero1@yahoo.com>
2017-12-28 20:25:59 +0200

    Update TestCalccedo.java

--- test/testcalccedo/TestCalccedo.java
+++ test/testcalccedo/TestCalccedo.java
@@ -15,7 +15,7 @@ public class TestCalccedo extends CalccedoHandler{
     public TestCalccedo(){
         super.init();
         // to show tracing operations of Calccedo
-        super.enableDeepTrace(true);
+        super.enableDeepTrace(false);
         
     }
 
@@ -25,11 +25,10 @@ public class TestCalccedo extends CalccedoHandler{
     
     public static void main(String[] args) {
        // this is sample formula  
-       String formula="1+1+2+3Sin(30)+10";
+       String formula="1+1+2+3sin(30)+10";
        
-       // instantiate object from CalccedoHandler 
-       CalccedoHandler calccedoHandler=new CalccedoHandler();
-       calccedoHandler.enableDeepTrace(true);
+       // instantiate object from TestCalccedo 
+       TestCalccedo calccedoHandler=new TestCalccedo();
 
        // now let's get result, just by calling calccedo calculate method
        System.out.println("Welcome to Calccedo Engine Library");
